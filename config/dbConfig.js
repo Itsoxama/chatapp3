8ce902f4ca67c98607e6abaab1cc3a5cd131167c
Itsoxama <108177301+Itsoxama@users.noreply.github.com>
2022-10-16 20:36:44 +0500

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 const uri = "mongodb+srv://admin:joho@cluster0.iykqmyl.mongodb.net/?retryWrites=true&w=majority";
 
 // connect to db
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri, { useNewUrlParser: true,useFindAndModify:false, useUnifiedTopology: true,
+
+
+  useCreateIndex: true, //make this true
+  autoIndex: true, //make this also true
+});
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
